ec1ff7602b4a101e50576741bf3c1dbcbeb14ae1
xixq-c <armaanrakhra@hotmail.com>
2026-09-21 23:04:43 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'latijn-schema-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './toets.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
